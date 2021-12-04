9cea6751e07c34a76716d3ba4452905c007a56d3
jvcataquiz <jvcataquiz@gmail.com>
2021-12-04 15:37:17 +0800

    Displaying Zero in seven Segment using Timer

--- SevenSegment/Timer/Form1.Designer.cs
+++ SevenSegment/Timer/Form1.Designer.cs
@@ -29,125 +29,124 @@ namespace Timer
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnSecMid = new System.Windows.Forms.Button();
             this.btnSecBotleft = new System.Windows.Forms.Button();
             this.btnSecBotright = new System.Windows.Forms.Button();
             this.btnSecTopRight = new System.Windows.Forms.Button();
             this.btnSecTopleft = new System.Windows.Forms.Button();
             this.btnSecTop = new System.Windows.Forms.Button();
-            this.btnSecBot = new System.Windows.Forms.Button();
-            this.btnBot2 = new System.Windows.Forms.Button();
-            this.btnTop2 = new System.Windows.Forms.Button();
             this.btnTop2right = new System.Windows.Forms.Button();
             this.btnTop2left = new System.Windows.Forms.Button();
             this.btnBot2right = new System.Windows.Forms.Button();
             this.btnBot2lleft = new System.Windows.Forms.Button();
             this.btnMid2 = new System.Windows.Forms.Button();
+            this.Dot = new System.Windows.Forms.Label();
+            this.btnBot = new System.Windows.Forms.Button();
+            this.btnTop2 = new System.Windows.Forms.Button();
+            this.btnBot2 = new System.Windows.Forms.Button();
+            this.timerSec = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // btnSecMid
             // 
-            this.btnSecMid.Location = new System.Drawing.Point(630, 189);
+            this.btnSecMid.Location = new System.Drawing.Point(649, 189);
             this.btnSecMid.Name = "btnSecMid";
-            this.btnSecMid.Size = new System.Drawing.Size(98, 23);
+            this.btnSecMid.Size = new System.Drawing.Size(60, 23);
             this.btnSecMid.TabIndex = 1;
+            this.btnSecMid.TabStop = false;
             this.btnSecMid.UseVisualStyleBackColor = true;
+            this.btnSecMid.Click += new System.EventHandler(this.SecondElements);
             // 
             // btnSecBotleft
             // 
-            this.btnSecBotleft.Location = new System.Drawing.Point(630, 211);
+            this.btnSecBotleft.Location = new System.Drawing.Point(630, 202);
             this.btnSecBotleft.Name = "btnSecBotleft";
-            this.btnSecBotleft.Size = new System.Drawing.Size(20, 70);
+            this.btnSecBotleft.Size = new System.Drawing.Size(20, 101);
             this.btnSecBotleft.TabIndex = 7;
+            this.btnSecBotleft.TabStop = false;
             this.btnSecBotleft.UseVisualStyleBackColor = true;
+            this.btnSecBotleft.Click += new System.EventHandler(this.SecondElements);
             // 
             // btnSecBotright
             // 
-            this.btnSecBotright.Location = new System.Drawing.Point(708, 211);
+            this.btnSecBotright.Location = new System.Drawing.Point(708, 202);
             this.btnSecBotright.Name = "btnSecBotright";
-            this.btnSecBotright.Size = new System.Drawing.Size(20, 70);
+            this.btnSecBotright.Size = new System.Drawing.Size(20, 101);
             this.btnSecBotright.TabIndex = 8;
+            this.btnSecBotright.TabStop = false;
             this.btnSecBotright.UseVisualStyleBackColor = true;
+            this.btnSecBotright.Click += new System.EventHandler(this.SecondElements);
             // 
             // btnSecTopRight
             // 
-            this.btnSecTopRight.Location = new System.Drawing.Point(708, 119);
+            this.btnSecTopRight.Location = new System.Drawing.Point(708, 98);
             this.btnSecTopRight.Name = "btnSecTopRight";
-            this.btnSecTopRight.Size = new System.Drawing.Size(20, 70);
+            this.btnSecTopRight.Size = new System.Drawing.Size(20, 107);
             this.btnSecTopRight.TabIndex = 11;
+            this.btnSecTopRight.TabStop = false;
             this.btnSecTopRight.UseVisualStyleBackColor = true;
+            this.btnSecTopRight.Click += new System.EventHandler(this.SecondElements);
             // 
             // btnSecTopleft
             // 
-            this.btnSecTopleft.Location = new System.Drawing.Point(630, 119);
+            this.btnSecTopleft.Location = new System.Drawing.Point(630, 98);
             this.btnSecTopleft.Name = "btnSecTopleft";
-            this.btnSecTopleft.Size = new System.Drawing.Size(20, 70);
+            this.btnSecTopleft.Size = new System.Drawing.Size(20, 107);
             this.btnSecTopleft.TabIndex = 10;
+            this.btnSecTopleft.TabStop = false;
             this.btnSecTopleft.UseVisualStyleBackColor = true;
+            this.btnSecTopleft.Click += new System.EventHandler(this.SecondElements);
             // 
             // btnSecTop
             // 
-            this.btnSecTop.Location = new System.Drawing.Point(630, 98);
+            this.btnSecTop.Location = new System.Drawing.Point(649, 98);
             this.btnSecTop.Name = "btnSecTop";
-            this.btnSecTop.Size = new System.Drawing.Size(98, 23);
+            this.btnSecTop.Size = new System.Drawing.Size(60, 23);
             this.btnSecTop.TabIndex = 12;
+            this.btnSecTop.TabStop = false;
             this.btnSecTop.UseVisualStyleBackColor = true;
-            // 
-            // btnSecBot
-            // 
-            this.btnSecBot.Location = new System.Drawing.Point(630, 280);
-            this.btnSecBot.Name = "btnSecBot";
-            this.btnSecBot.Size = new System.Drawing.Size(98, 23);
-            this.btnSecBot.TabIndex = 13;
-            this.btnSecBot.UseVisualStyleBackColor = true;
-            // 
-            // btnBot2
-            // 
-            this.btnBot2.Location = new System.Drawing.Point(526, 280);
-            this.btnBot2.Name = "btnBot2";
-            this.btnBot2.Size = new System.Drawing.Size(98, 23);
-            this.btnBot2.TabIndex = 20;
-            this.btnBot2.UseVisualStyleBackColor = true;
-            // 
-            // btnTop2
-            // 
-            this.btnTop2.Location = new System.Drawing.Point(526, 98);
-            this.btnTop2.Name = "btnTop2";
-            this.btnTop2.Size = new System.Drawing.Size(98, 23);
-            this.btnTop2.TabIndex = 19;
-            this.btnTop2.UseVisualStyleBackColor = true;
+            this.btnSecTop.Click += new System.EventHandler(this.SecondElements);
             // 
             // btnTop2right
             // 
-            this.btnTop2right.Location = new System.Drawing.Point(604, 119);
+            this.btnTop2right.Location = new System.Drawing.Point(604, 98);
             this.btnTop2right.Name = "btnTop2right";
-            this.btnTop2right.Size = new System.Drawing.Size(20, 70);
+            this.btnTop2right.Size = new System.Drawing.Size(20, 91);
             this.btnTop2right.TabIndex = 18;
+            this.btnTop2right.TabStop = false;
             this.btnTop2right.UseVisualStyleBackColor = true;
+            this.btnTop2right.Click += new System.EventHandler(this.SecondElements);
             // 
             // btnTop2left
             // 
-            this.btnTop2left.Location = new System.Drawing.Point(526, 119);
+            this.btnTop2left.Location = new System.Drawing.Point(526, 98);
             this.btnTop2left.Name = "btnTop2left";
-            this.btnTop2left.Size = new System.Drawing.Size(20, 70);
+            this.btnTop2left.Size = new System.Drawing.Size(20, 91);
             this.btnTop2left.TabIndex = 17;
+            this.btnTop2left.TabStop = false;
             this.btnTop2left.UseVisualStyleBackColor = true;
+            this.btnTop2left.Click += new System.EventHandler(this.SecondElements);
             // 
             // btnBot2right
             // 
             this.btnBot2right.Location = new System.Drawing.Point(604, 211);
             this.btnBot2right.Name = "btnBot2right";
-            this.btnBot2right.Size = new System.Drawing.Size(20, 70);
+            this.btnBot2right.Size = new System.Drawing.Size(20, 92);
             this.btnBot2right.TabIndex = 16;
+            this.btnBot2right.TabStop = false;
             this.btnBot2right.UseVisualStyleBackColor = true;
+            this.btnBot2right.Click += new System.EventHandler(this.SecondElements);
             // 
             // btnBot2lleft
             // 
             this.btnBot2lleft.Location = new System.Drawing.Point(526, 211);
             this.btnBot2lleft.Name = "btnBot2lleft";
-            this.btnBot2lleft.Size = new System.Drawing.Size(20, 70);
+            this.btnBot2lleft.Size = new System.Drawing.Size(20, 92);
             this.btnBot2lleft.TabIndex = 15;
+            this.btnBot2lleft.TabStop = false;
             this.btnBot2lleft.UseVisualStyleBackColor = true;
+            this.btnBot2lleft.Click += new System.EventHandler(this.SecondElements);
             // 
             // btnMid2
             // 
@@ -155,7 +154,53 @@ namespace Timer
             this.btnMid2.Name = "btnMid2";
             this.btnMid2.Size = new System.Drawing.Size(98, 23);
             this.btnMid2.TabIndex = 14;
+            this.btnMid2.TabStop = false;
             this.btnMid2.UseVisualStyleBackColor = true;
+            this.btnMid2.Click += new System.EventHandler(this.SecondElements);
+            // 
+            // Dot
+            // 
+            this.Dot.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Dot.Location = new System.Drawing.Point(485, 268);
+            this.Dot.Name = "Dot";
+            this.Dot.Size = new System.Drawing.Size(35, 35);
+            this.Dot.TabIndex = 21;
+            // 
+            // btnBot
+            // 
+            this.btnBot.Location = new System.Drawing.Point(649, 280);
+            this.btnBot.Name = "btnBot";
+            this.btnBot.Size = new System.Drawing.Size(60, 23);
+            this.btnBot.TabIndex = 22;
+            this.btnBot.TabStop = false;
+            this.btnBot.UseVisualStyleBackColor = true;
+            this.btnBot.Click += new System.EventHandler(this.SecondElements);
+            // 
+            // btnTop2
+            // 
+            this.btnTop2.Location = new System.Drawing.Point(545, 98);
+            this.btnTop2.Name = "btnTop2";
+            this.btnTop2.Size = new System.Drawing.Size(60, 23);
+            this.btnTop2.TabIndex = 23;
+            this.btnTop2.TabStop = false;
+            this.btnTop2.UseVisualStyleBackColor = true;
+            this.btnTop2.Click += new System.EventHandler(this.SecondElements);
+            // 
+            // btnBot2
+            // 
+            this.btnBot2.Location = new System.Drawing.Point(545, 280);
+            this.btnBot2.Name = "btnBot2";
+            this.btnBot2.Size = new System.Drawing.Size(60, 23);
+            this.btnBot2.TabIndex = 24;
+            this.btnBot2.TabStop = false;
+            this.btnBot2.UseVisualStyleBackColor = true;
+            this.btnBot2.Click += new System.EventHandler(this.SecondElements);
+            // 
+            // timerSec
+            // 
+            this.timerSec.Enabled = true;
+            this.timerSec.Interval = 1000;
+            this.timerSec.Tick += new System.EventHandler(this.timerSec_Tick);
             // 
             // Form1
             // 
@@ -164,12 +209,13 @@ namespace Timer
             this.ClientSize = new System.Drawing.Size(760, 485);
             this.Controls.Add(this.btnBot2);
             this.Controls.Add(this.btnTop2);
+            this.Controls.Add(this.btnBot);
+            this.Controls.Add(this.Dot);
             this.Controls.Add(this.btnTop2right);
             this.Controls.Add(this.btnTop2left);
             this.Controls.Add(this.btnBot2right);
             this.Controls.Add(this.btnBot2lleft);
             this.Controls.Add(this.btnMid2);
-            this.Controls.Add(this.btnSecBot);
             this.Controls.Add(this.btnSecTop);
             this.Controls.Add(this.btnSecTopRight);
             this.Controls.Add(this.btnSecTopleft);
@@ -190,14 +236,16 @@ namespace Timer
         private System.Windows.Forms.Button btnSecTopRight;
         private System.Windows.Forms.Button btnSecTopleft;
         private System.Windows.Forms.Button btnSecTop;
-        private System.Windows.Forms.Button btnSecBot;
-        private System.Windows.Forms.Button btnBot2;
-        private System.Windows.Forms.Button btnTop2;
         private System.Windows.Forms.Button btnTop2right;
         private System.Windows.Forms.Button btnTop2left;
         private System.Windows.Forms.Button btnBot2right;
         private System.Windows.Forms.Button btnBot2lleft;
         private System.Windows.Forms.Button btnMid2;
+        private System.Windows.Forms.Label Dot;
+        private System.Windows.Forms.Button btnBot;
+        private System.Windows.Forms.Button btnTop2;
+        private System.Windows.Forms.Button btnBot2;
+        private System.Windows.Forms.Timer timerSec;
     }
 }
 
--- SevenSegment/Timer/Form1.cs
+++ SevenSegment/Timer/Form1.cs
@@ -12,9 +12,50 @@ namespace Timer
 {
     public partial class Form1 : Form
     {
+        int Tick = 0;
+        bool Color = true;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timerSec_Tick(object sender, EventArgs e)
+        {
+
+            Button newbot = new Button();
+
+         
+            if (Tick == 0)
+            {
+                 btnSecTop.PerformClick();
+                 btnSecTopleft.PerformClick();
+                 btnSecTopRight.PerformClick();
+                 btnSecBotright.PerformClick();
+                 btnSecBotleft.PerformClick();
+                 btnBot.PerformClick();
+            }
+            else
+            {
+                btnSecTop.UseVisualStyleBackColor = true;
+                btnSecTopleft.UseVisualStyleBackColor = true;
+                btnSecTopRight.UseVisualStyleBackColor = true;
+                btnSecBotright.UseVisualStyleBackColor = true;
+                btnSecBotleft.UseVisualStyleBackColor = true;
+                btnBot.UseVisualStyleBackColor = true;
+               
+            }
+            Tick++;
+        }
+
+        private void SecondElements(object sender, EventArgs e)
+        {
+            Button btnclick = (Button)sender;
+            btnclick.BackColor = System.Drawing.Color.Red;
+           
+        }
+        private void changer()
+        {
+          
         }
     }
 }
