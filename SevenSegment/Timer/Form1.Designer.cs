4271b9c8785dce4ee1a2f1c146895631c29c4b26
jvcataquiz <jvcataquiz@gmail.com>
2021-12-04 14:29:07 +0800

    Creating Buttons For Second

---
+++ SevenSegment/Timer/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSecMid = new System.Windows.Forms.Button();
+            this.btnSecBotleft = new System.Windows.Forms.Button();
+            this.btnSecBotright = new System.Windows.Forms.Button();
+            this.btnSecTopRight = new System.Windows.Forms.Button();
+            this.btnSecTopleft = new System.Windows.Forms.Button();
+            this.btnSecTop = new System.Windows.Forms.Button();
+            this.btnSecBot = new System.Windows.Forms.Button();
+            this.btnBot2 = new System.Windows.Forms.Button();
+            this.btnTop2 = new System.Windows.Forms.Button();
+            this.btnTop2right = new System.Windows.Forms.Button();
+            this.btnTop2left = new System.Windows.Forms.Button();
+            this.btnBot2right = new System.Windows.Forms.Button();
+            this.btnBot2lleft = new System.Windows.Forms.Button();
+            this.btnMid2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSecMid
+            // 
+            this.btnSecMid.Location = new System.Drawing.Point(630, 189);
+            this.btnSecMid.Name = "btnSecMid";
+            this.btnSecMid.Size = new System.Drawing.Size(98, 23);
+            this.btnSecMid.TabIndex = 1;
+            this.btnSecMid.UseVisualStyleBackColor = true;
+            // 
+            // btnSecBotleft
+            // 
+            this.btnSecBotleft.Location = new System.Drawing.Point(630, 211);
+            this.btnSecBotleft.Name = "btnSecBotleft";
+            this.btnSecBotleft.Size = new System.Drawing.Size(20, 70);
+            this.btnSecBotleft.TabIndex = 7;
+            this.btnSecBotleft.UseVisualStyleBackColor = true;
+            // 
+            // btnSecBotright
+            // 
+            this.btnSecBotright.Location = new System.Drawing.Point(708, 211);
+            this.btnSecBotright.Name = "btnSecBotright";
+            this.btnSecBotright.Size = new System.Drawing.Size(20, 70);
+            this.btnSecBotright.TabIndex = 8;
+            this.btnSecBotright.UseVisualStyleBackColor = true;
+            // 
+            // btnSecTopRight
+            // 
+            this.btnSecTopRight.Location = new System.Drawing.Point(708, 119);
+            this.btnSecTopRight.Name = "btnSecTopRight";
+            this.btnSecTopRight.Size = new System.Drawing.Size(20, 70);
+            this.btnSecTopRight.TabIndex = 11;
+            this.btnSecTopRight.UseVisualStyleBackColor = true;
+            // 
+            // btnSecTopleft
+            // 
+            this.btnSecTopleft.Location = new System.Drawing.Point(630, 119);
+            this.btnSecTopleft.Name = "btnSecTopleft";
+            this.btnSecTopleft.Size = new System.Drawing.Size(20, 70);
+            this.btnSecTopleft.TabIndex = 10;
+            this.btnSecTopleft.UseVisualStyleBackColor = true;
+            // 
+            // btnSecTop
+            // 
+            this.btnSecTop.Location = new System.Drawing.Point(630, 98);
+            this.btnSecTop.Name = "btnSecTop";
+            this.btnSecTop.Size = new System.Drawing.Size(98, 23);
+            this.btnSecTop.TabIndex = 12;
+            this.btnSecTop.UseVisualStyleBackColor = true;
+            // 
+            // btnSecBot
+            // 
+            this.btnSecBot.Location = new System.Drawing.Point(630, 280);
+            this.btnSecBot.Name = "btnSecBot";
+            this.btnSecBot.Size = new System.Drawing.Size(98, 23);
+            this.btnSecBot.TabIndex = 13;
+            this.btnSecBot.UseVisualStyleBackColor = true;
+            // 
+            // btnBot2
+            // 
+            this.btnBot2.Location = new System.Drawing.Point(526, 280);
+            this.btnBot2.Name = "btnBot2";
+            this.btnBot2.Size = new System.Drawing.Size(98, 23);
+            this.btnBot2.TabIndex = 20;
+            this.btnBot2.UseVisualStyleBackColor = true;
+            // 
+            // btnTop2
+            // 
+            this.btnTop2.Location = new System.Drawing.Point(526, 98);
+            this.btnTop2.Name = "btnTop2";
+            this.btnTop2.Size = new System.Drawing.Size(98, 23);
+            this.btnTop2.TabIndex = 19;
+            this.btnTop2.UseVisualStyleBackColor = true;
+            // 
+            // btnTop2right
+            // 
+            this.btnTop2right.Location = new System.Drawing.Point(604, 119);
+            this.btnTop2right.Name = "btnTop2right";
+            this.btnTop2right.Size = new System.Drawing.Size(20, 70);
+            this.btnTop2right.TabIndex = 18;
+            this.btnTop2right.UseVisualStyleBackColor = true;
+            // 
+            // btnTop2left
+            // 
+            this.btnTop2left.Location = new System.Drawing.Point(526, 119);
+            this.btnTop2left.Name = "btnTop2left";
+            this.btnTop2left.Size = new System.Drawing.Size(20, 70);
+            this.btnTop2left.TabIndex = 17;
+            this.btnTop2left.UseVisualStyleBackColor = true;
+            // 
+            // btnBot2right
+            // 
+            this.btnBot2right.Location = new System.Drawing.Point(604, 211);
+            this.btnBot2right.Name = "btnBot2right";
+            this.btnBot2right.Size = new System.Drawing.Size(20, 70);
+            this.btnBot2right.TabIndex = 16;
+            this.btnBot2right.UseVisualStyleBackColor = true;
+            // 
+            // btnBot2lleft
+            // 
+            this.btnBot2lleft.Location = new System.Drawing.Point(526, 211);
+            this.btnBot2lleft.Name = "btnBot2lleft";
+            this.btnBot2lleft.Size = new System.Drawing.Size(20, 70);
+            this.btnBot2lleft.TabIndex = 15;
+            this.btnBot2lleft.UseVisualStyleBackColor = true;
+            // 
+            // btnMid2
+            // 
+            this.btnMid2.Location = new System.Drawing.Point(526, 189);
+            this.btnMid2.Name = "btnMid2";
+            this.btnMid2.Size = new System.Drawing.Size(98, 23);
+            this.btnMid2.TabIndex = 14;
+            this.btnMid2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 485);
+            this.Controls.Add(this.btnBot2);
+            this.Controls.Add(this.btnTop2);
+            this.Controls.Add(this.btnTop2right);
+            this.Controls.Add(this.btnTop2left);
+            this.Controls.Add(this.btnBot2right);
+            this.Controls.Add(this.btnBot2lleft);
+            this.Controls.Add(this.btnMid2);
+            this.Controls.Add(this.btnSecBot);
+            this.Controls.Add(this.btnSecTop);
+            this.Controls.Add(this.btnSecTopRight);
+            this.Controls.Add(this.btnSecTopleft);
+            this.Controls.Add(this.btnSecBotright);
+            this.Controls.Add(this.btnSecBotleft);
+            this.Controls.Add(this.btnSecMid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSecMid;
+        private System.Windows.Forms.Button btnSecBotleft;
+        private System.Windows.Forms.Button btnSecBotright;
+        private System.Windows.Forms.Button btnSecTopRight;
+        private System.Windows.Forms.Button btnSecTopleft;
+        private System.Windows.Forms.Button btnSecTop;
+        private System.Windows.Forms.Button btnSecBot;
+        private System.Windows.Forms.Button btnBot2;
+        private System.Windows.Forms.Button btnTop2;
+        private System.Windows.Forms.Button btnTop2right;
+        private System.Windows.Forms.Button btnTop2left;
+        private System.Windows.Forms.Button btnBot2right;
+        private System.Windows.Forms.Button btnBot2lleft;
+        private System.Windows.Forms.Button btnMid2;
+    }
+}
+
